178322fc324c6e49da47fda0b7cdc644c23b086a
Elijus Žekonis <56361945+Elijuszek@users.noreply.github.com>
2024-11-04 20:02:05 +0200

    JWT auth

--- services/auth/jwt.go
+++ services/auth/jwt.go
@@ -2,11 +2,73 @@ package auth
 
 import (
 	"educations-castle/configs"
+	"educations-castle/types"
+	"educations-castle/utils"
+	"fmt"
+	"log"
+	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/golang-jwt/jwt"
+	"golang.org/x/net/context"
 )
+
+type contextKey string
+
+const UserKey contextKey = "userID"
+
+func WithJWTAuth(handlerFunc http.HandlerFunc, castle types.UserCastle) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := utils.GetTokenFromRequest(r)
+		token, err := ValidateJWT(tokenString)
+
+		if err != nil {
+			log.Printf("Failed to validate token: %v", err)
+			PermissionDenied(w)
+			return
+		}
+		if !token.Valid {
+			log.Printf("Invalid token")
+			PermissionDenied(w)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		str := claims["userID"].(string)
+		userID, err := strconv.Atoi(str)
+		if err != nil {
+			log.Printf("Failed to convert UserID to int: %v", err)
+			PermissionDenied(w)
+			return
+		}
+
+		u, err := castle.GetUserByID(userID)
+		if err != nil {
+			log.Printf("Failed to get user by id: %v", err)
+			PermissionDenied(w)
+			return
+		}
+
+		// Add the user to the context
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, UserKey, u.ID)
+		r = r.WithContext(ctx)
+
+		// If token is valid
+		handlerFunc(w, r)
+	}
+}
+
+func ValidateJWT(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(configs.Envs.JWTSecret), nil
+	})
+}
 
 func CreateJWT(secret []byte, userID int) (string, error) {
 	expiration := time.Second * time.Duration(configs.Envs.JWTExpirationInSeconds)
@@ -21,4 +83,17 @@ func CreateJWT(secret []byte, userID int) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func PermissionDenied(w http.ResponseWriter) {
+	utils.WriteError(w, http.StatusForbidden, fmt.Errorf("permission denied"))
+}
+
+func GetUserIDFromContext(ctx context.Context) int {
+	userID, ok := ctx.Value(UserKey).(int)
+	if !ok {
+		return -1
+	}
+
+	return userID
 }
