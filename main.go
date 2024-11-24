600d2c8b4751c3997f0bb525d60c6a9151446800
Elijus Žekonis <56361945+Elijuszek@users.noreply.github.com>
2024-11-24 03:54:05 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"database/sql"
 	"educations-castle/cmd/api"
 	"educations-castle/configs"
@@ -10,28 +8,11 @@ import (
 	"educations-castle/utils/color"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// Load the CA certificate from the path specified in the configuration
-	rootCertPool := x509.NewCertPool()
-	pem, err := os.ReadFile(configs.Envs.CACertPath) // Assuming CACertPath is the config field
-	if err != nil {
-		log.Fatalf("Failed to read CA certificate from path %s: %v", configs.Envs.CACertPath, err)
-	}
-
-	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
-		log.Fatal("Failed to append CA certificate")
-	}
-
-	// Register a custom tls.Config
-	mysql.RegisterTLSConfig("custom", &tls.Config{
-		RootCAs: rootCertPool,
-	})
-
 	// Use the custom TLS config in the DB connection
 	db, err := db.NewMySQLStorage(mysql.Config{
 		User:                 configs.Envs.DBUser,
@@ -39,7 +20,6 @@ func main() {
 		Addr:                 configs.Envs.DBAddress,
 		DBName:               configs.Envs.DBName,
 		Net:                  "tcp",
-		TLSConfig:            "custom", // This references the TLS config we registered
 		AllowNativePasswords: true,
 		ParseTime:            true,
 	})
