f04b11d399904fb8de061886231098ef76b59cd3
Elijus Žekonis <56361945+Elijuszek@users.noreply.github.com>
2024-12-16 04:42:55 +0200

    Update api.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -56,7 +56,7 @@ func (s *APIServer) Run() error {
 // CORS middleware to add CORS headers to the HTTP response
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
